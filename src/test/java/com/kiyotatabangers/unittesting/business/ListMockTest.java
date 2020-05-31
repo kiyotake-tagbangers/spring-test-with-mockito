afbfd21aab178f7c223160b6f199b0a9138a119b
kiyota takeshi <kiyota@tagbangers.co.jp>
2020-05-31 12:26:14 +0900

    refactoring and multiple return values test

--- src/test/java/com/kiyotatabangers/unittesting/business/ListMockTest.java
+++ src/test/java/com/kiyotatabangers/unittesting/business/ListMockTest.java
@@ -9,18 +9,27 @@ import static org.mockito.Mockito.*;
 
 public class ListMockTest {
 
+    List mock = mock(List.class);
+
     @Test
     public void size_basic(){
-        List mock = mock(List.class);
         when(mock.size()).thenReturn(5);
         assertEquals(5, mock.size());
     }
 
     @Test
     public void returnDifferentValues(){
-        List mock = mock(List.class);
         when(mock.size()).thenReturn(5).thenReturn(10);
         assertEquals(5, mock.size());
         assertEquals(10, mock.size());
+    }
+
+    @Test
+    public void returnWithParameters(){
+        when(mock.get(0)).thenReturn("Test");
+        when(mock.get(1)).thenReturn("Test1");
+        assertEquals("Test",mock.get(0));
+        assertEquals("Test1",mock.get(1));
+        assertEquals(null,mock.get(2));
     }
 }
