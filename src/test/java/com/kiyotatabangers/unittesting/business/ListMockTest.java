c270199ad8233e3f221d419dace34c4adfaaaaa8
kiyota takeshi <kiyota@tagbangers.co.jp>
2020-05-31 12:33:21 +0900

    using anyInt argument matcher

--- src/test/java/com/kiyotatabangers/unittesting/business/ListMockTest.java
+++ src/test/java/com/kiyotatabangers/unittesting/business/ListMockTest.java
@@ -12,24 +12,31 @@ public class ListMockTest {
     List mock = mock(List.class);
 
     @Test
-    public void size_basic(){
+    public void size_basic() {
         when(mock.size()).thenReturn(5);
         assertEquals(5, mock.size());
     }
 
     @Test
-    public void returnDifferentValues(){
+    public void returnDifferentValues() {
         when(mock.size()).thenReturn(5).thenReturn(10);
         assertEquals(5, mock.size());
         assertEquals(10, mock.size());
     }
 
     @Test
-    public void returnWithParameters(){
+    public void returnWithParameters() {
         when(mock.get(0)).thenReturn("Test");
         when(mock.get(1)).thenReturn("Test1");
-        assertEquals("Test",mock.get(0));
-        assertEquals("Test1",mock.get(1));
-        assertEquals(null,mock.get(2));
+        assertEquals("Test", mock.get(0));
+        assertEquals("Test1", mock.get(1));
+        assertEquals(null, mock.get(2));
+    }
+
+    @Test
+    public void returnWithGenericParameters() {
+        when(mock.get(anyInt())).thenReturn("Test");
+        assertEquals("Test", mock.get(0));
+        assertEquals("Test", mock.get(1));
     }
 }
